f4f27f2b6aa35bc6232805a7863d1847ae9d1349
MahsaSnt <mahsa.snti@gmail.com>
2021-05-03 14:40:08 +0430

    adding files

--- app0.py
+++ app0.py
@@ -1,21 +1,20 @@
-from flask import request, redirect,Flask,render_template,url_for,redirect,Response
-from flask_bootstrap import Bootstrap
-from flask import send_file
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+#from my own code
 from async_digikala import output
 
 app=Flask(__name__)
-Bootstrap(app)
+CORS(app)
 
-@app.route('/',methods=['GET','POST'])
-def c():
-    if request.method=="POST":
-            total=int(request.form['total'])
-            subject=request.form['subject']
-            output(total,subject)
-    return render_template('form.html') 
-  
+@app.route('/api',methods=['POST'])
+def index():
+    data = request.get_json()
+    subject = data["subject"]
+    num_pages = data["num_pages"]
+    products = output(num_pages,subject)
+    return jsonify(products= f"{products}")
+    
+        
 if __name__ == '__main__':
     app.run(debug=True)
-    
-#print(output(4,'گوشی'))  
     
